1cf2244828fc7963df84ef1a67413a294c44b478
Feq149 <Konrad.ciborowski@student.uj.edu.pl>
2023-05-27 23:30:30 +0200

    pr

--- core/src/level/Level.java
+++ core/src/level/Level.java
@@ -3,6 +3,7 @@ package level;
 import characters.Enemy;
 import characters.Player;
 import map.Board;
+import projectiles.Projectile;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +12,7 @@ public class Level {
     public Board board;
     public List<Enemy> enemies = new ArrayList<>();
     public Player player;
-
+    public List<Projectile> projectiles = new ArrayList<>();
     public Level(int difficulty) {
         board = new Board();
     }
