c506d7d96d15f0c6ff27728c5b02aefb8684719b
Feq149 <Konrad.ciborowski@student.uj.edu.pl>
2023-05-27 19:23:53 +0200

    enemySpawnerBlock4

--- core/src/specficBlockOfTiles/EnemySpawnerBlock3.java
+++ core/src/specficBlockOfTiles/EnemySpawnerBlock3.java
@@ -8,7 +8,7 @@ public class EnemySpawnerBlock3 extends BlockOfTiles {
     EnemySpawnerBlock3(int xCenterCoordinate, int yCenterCoordinate) {
         setxCenterCoordinate(xCenterCoordinate);
         setyCenterCoordinate(yCenterCoordinate);
-        internalTiles.add(new HealingCamp((int)getxCenterCoordinate() - Tile.width , (int)getyCenterCoordinate() + Tile.length));
+        internalTiles.add(new Spikes((int)getxCenterCoordinate() - Tile.width , (int)getyCenterCoordinate() + Tile.length));
         internalTiles.add(new EnemySpawner((int)getxCenterCoordinate() , (int)getyCenterCoordinate() + Tile.length));
         internalTiles.add(new EnemySpawner((int)getxCenterCoordinate() + Tile.width , (int)getyCenterCoordinate() + Tile.length));
         internalTiles.add(new NormalTile((int)getxCenterCoordinate() - Tile.width , (int)getyCenterCoordinate()));
--- core/src/specficBlockOfTiles/EnemySpawnerBlock4.java
+++ core/src/specficBlockOfTiles/EnemySpawnerBlock4.java
@@ -1,10 +1,22 @@
 package specficBlockOfTiles;
 
 import map.BlockOfTiles;
+import map.Tile;
+import specificTiles.EnemySpawner;
+import specificTiles.NormalTile;
 
 public class EnemySpawnerBlock4 extends BlockOfTiles {
     EnemySpawnerBlock4(int xCenterCoordinate, int yCenterCoordinate) {
         setxCenterCoordinate(xCenterCoordinate);
         setyCenterCoordinate(yCenterCoordinate);
+        internalTiles.add(new EnemySpawner((int)getxCenterCoordinate() - Tile.width , (int)getyCenterCoordinate() + Tile.length));
+        internalTiles.add(new EnemySpawner((int)getxCenterCoordinate() , (int)getyCenterCoordinate() + Tile.length));
+        internalTiles.add(new NormalTile((int)getxCenterCoordinate() + Tile.width , (int)getyCenterCoordinate() + Tile.length));
+        internalTiles.add(new NormalTile((int)getxCenterCoordinate() - Tile.width , (int)getyCenterCoordinate()));
+        internalTiles.add(new EnemySpawner((int)getxCenterCoordinate(), (int)getyCenterCoordinate()));
+        internalTiles.add(new EnemySpawner((int)getxCenterCoordinate() + Tile.width , (int)getyCenterCoordinate()));
+        internalTiles.add(new NormalTile((int)getxCenterCoordinate() - Tile.width , (int)getyCenterCoordinate() - Tile.length));
+        internalTiles.add(new EnemySpawner((int)getxCenterCoordinate() , (int)getyCenterCoordinate() - Tile.length));
+        internalTiles.add(new EnemySpawner((int)getxCenterCoordinate() + Tile.width , (int)getyCenterCoordinate() - Tile.length));
     }
 }
