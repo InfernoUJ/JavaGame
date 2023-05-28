d80e062b763089e4fee34432c22a4cce791f0535
InfernoUJ <mykhailo.petin@student.uj.edu.pl>
2023-05-28 21:32:17 +0200

    started implementing Keyboard Events handling

--- desktop/src/view/GameScreen.java
+++ desktop/src/view/GameScreen.java
@@ -3,18 +3,31 @@ package view;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import viewmodel.GameManager;
 import viewmodel.Manager;
 
 public class GameScreen extends StageWrapper{
     private GameManager gameManager;
+    private Hero hero;
+
     public GameScreen(GameManager gameManager) {
         super();
         this.gameManager = gameManager;
-        currentStage = new Stage();
+        currentStage = new Stage(gameManager.getViewport());
+        hero = new Hero(gameManager);
+        hero.addListener(new InputListener(){
+            @Override
+            public boolean keyDown(com.badlogic.gdx.scenes.scene2d.InputEvent event, int keycode) {
+                hero.gameManager.processKeyDown(keycode);
+                return true;
+            }
+        });
     }
+
 
     private void loadScene(){
 
